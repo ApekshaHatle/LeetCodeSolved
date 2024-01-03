d70b93519d5157bcdbc2c522ace53274a23c6599
Apeksha Hatle <apekshahatle@gmail.com>
2024-01-03 16:37:43 +0530

    Jan 3

---
+++ NumberofLaserBeamsinaBank_2125.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        
+        vector<int> edge={};
+        int rows = bank.size();
+        int cols = (rows > 0 && !bank[0].empty()) ? bank[0].length() : 0;
+        
+        if(rows==1)
+            return 0;
+        for (int i = 0; i < rows; ++i) 
+        {
+            int c=0;
+            for (int j = 0; j < cols; ++j) 
+            {
+                if(bank[i][j] =='1')
+                    c++;
+            }
+            
+            if(c==0)
+            {
+                continue;
+            }
+            else 
+                edge.push_back(c);
+        }
+        
+        if(edge.empty())
+        {
+            edge.push_back(0);
+        }
+        int total=0;
+        for(int i = 0 ; i < edge.size()-1 ;i++ )
+        {
+            total=total+(edge[i]*edge[i+1]);
+        }
+        
+        return total;
+    }
+};
